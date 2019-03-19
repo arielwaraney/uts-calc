278b28e5ab44d6006a4c547b95544839fe3b5ba9
arielwaraney <79386657+arielwaraney@users.noreply.github.com>
2019-03-19 22:44:42 +1100

    Adding the calculator basic code from downloadable file from Canvas

--- calc/main.swift
+++ calc/main.swift
@@ -12,3 +12,23 @@ var args = ProcessInfo.processInfo.arguments
 args.removeFirst() // remove the name of the program
 
 print(Int(args[0])!)
+
+if args.isEmpty {
+    print("Empty arguments")
+    exit(1)
+}
+
+// Retrieve User Input
+//let no1 = args[0]; // Sample Code Only! Update Required!
+//let operator = args[1]; // Sample Code Only! Update Required!
+//let no2 = args[2]; // Sample Code Only! Update Required!
+
+
+//initialize calculator object
+let calculator = Calculator()
+
+// Calculate the result
+let result = calculator.add(no1: 1, no2: 1);
+
+print(result)
+
